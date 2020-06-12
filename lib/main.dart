06ae261483a66bd070c30c082210d371f714a7a6
sharanrohitrajnatarajan <srrnatar@gmail.com>
2020-06-12 10:39:02 -0400

    AppCenter changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:jaldiio/Models/user.dart';
 import 'package:jaldiio/Services/auth.dart';
@@ -8,11 +9,18 @@ import 'SignUpPage.dart';
 import 'WelcomePage.dart';
 import 'ForgotPassword.dart';
 import 'Wrapper.dart';
+import 'package:appcenter/appcenter.dart';
+import 'package:appcenter_analytics/appcenter_analytics.dart';
+import 'package:appcenter_crashes/appcenter_crashes.dart';
+
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
+
     return StreamProvider<User>.value(
       value: AuthService().user_stream,
 
